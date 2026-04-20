350e2c2e45b74ce330d01830df8a38c864a23787
DavideMara <davidemaracaglia@yahoo.com>
2026-04-20 12:17:11 +0200

    finishing

---
+++ EscSimilEsonero24/src/ListaElementi.java
@@ -0,0 +1,8 @@
+public class ListaElementi {
+    private Nodo testa;
+    public Nodo getTesta(){
+        return testa;
+    }
+
+
+}
---
+++ EscSimilEsonero24/src/Nodo.java
@@ -0,0 +1,9 @@
+class Nodo {
+    String info;
+    Nodo next;
+
+    public Nodo(String info, Nodo next) {
+        this.info = info;
+        this.next = next;
+    }
+}
